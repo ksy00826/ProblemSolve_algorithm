4d949027a50c7c716dd57b1172ae96ef51682542
ksy00826 <76732805+ksy00826@users.noreply.github.com>
2023-02-07 22:13:32 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuffer sb = new StringBuffer();
+	static int[] status;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		//input
+		int N = Integer.parseInt(in.readLine());
+		status = new int[N + 1];
+		String[] arr = in.readLine().split(" ");
+		for (int i = 0; i < arr.length; i++) status[i + 1] = Integer.parseInt(arr[i]);
+		
+		int stuN = Integer.parseInt(in.readLine());
+		
+		while(stuN-- > 0) {
+			//inputStu
+			arr = in.readLine().split(" ");
+			String who = arr[0];
+			int num = Integer.parseInt(arr[1]);
+			
+			if (who.equals("1")) {
+				//남자
+				changeMan(num);
+			}
+			else {
+				//여자
+				changeWoman(num);
+			}
+			
+		}
+		
+		int idx = 1;
+		while(true) {
+			if (idx == status.length) break;
+			
+			sb.append(status[idx]);
+			if (idx % 20 == 0) sb.append('\n');
+			else sb.append(' ');
+			idx++;
+		}
+		System.out.println(sb);
+	}
+
+	private static void changeWoman(int num) {
+		int i = 0;
+		while(true) {
+			if ((num - i >= 1 && num + i < status.length) //인덱스 0은 제외
+					&& (status[num - i] == status[num + i])) {
+				onOff(num - i);
+				onOff(num + i);
+				i++;
+			}
+			else break;
+		}
+		onOff(num); //가운데는 중복되니까
+	}
+
+	private static void changeMan(int num) {
+		for (int i = 0; i < status.length; i++) {
+			if (i % num == 0) onOff(i);
+		}
+	}
+
+	private static void onOff(int i) {
+		status[i] = (status[i] == 0) ? 1 : 0;
+	}
+}

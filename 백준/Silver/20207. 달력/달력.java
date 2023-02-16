d9bb794cda94d548340883b9f2fc5895dfcd6e0b
ksy00826 <76732805+ksy00826@users.noreply.github.com>
2023-02-16 11:15:41 +0900

    [Silver I] Title: 달력, Time: 140 ms, Memory: 15288 KB -BaekjoonHub

--- 백준/Silver/20207. 달력/달력.java
+++ 백준/Silver/20207. 달력/달력.java
@@ -1,30 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	/*
-	 * 우선 날짜들을 다 입력받고 다음과 같은 기준으로 정렬한다.
-	 * 1. 시작일이 빠른 것
-	 * 2. 시작일이 같다면, 일정 기간이 긴 것
-	 * 
-	 * 아니다
-	 * 그냥 day[365] 만들고
-	 * 시작일과 종료일 입력받으면서
-	 * 카운트하고
-	 * 가로 = 연속적으로 0이 아닌 길이
-	 * 세로 = 겹치는 최대 일정 수
-	 */
 	static int dayN = 366; //인덱스 에러
 	static int[] day = new int[dayN]; //계속 초기화 까먹음
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+//		Scanner sc = new Scanner(System.in);
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(in.readLine());
 		
 		//모든 날 입력받으면서 ++
 		while (N-- > 0) {
-			int start = sc.nextInt();
-			int end = sc.nextInt();
+			String[] line = in.readLine().split(" ");
+			int start = Integer.parseInt(line[0]);
+			int end = Integer.parseInt(line[1]);
 			
 			addArray(start, end);
 		}
@@ -44,7 +37,7 @@ public class Main {
 				h = 0;
 			}
 		}
-		//365에서! 0이 아니면 면적 업뎃 안되니까
+		//365에서! 0이 아니면 면적 업뎃 안되니까 -- 됐당! -- 처음과 끝 조건 잘 확인해야 함!!!
 		if (day[365] != 0) {
 			area += (w * h);
 		}

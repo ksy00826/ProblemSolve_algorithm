ce36d65ea8902fec04e4218c7492757211a94e53
ksy00826 <76732805+ksy00826@users.noreply.github.com>
2023-02-18 00:45:07 +0900

    [Gold V] Title: A와 B 2, Time: 184 ms, Memory: 24588 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static String S, T;
+	static boolean isChange = false;
+	static int cntT;
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		S = in.readLine();
+		T = in.readLine();
+		if (subStr(S)) {
+			cntT = getACnt(T);
+			dfs(S, getACnt(S));
+		}
+		/*
+		 * 시간초과가 좀 날 거 같음.. 
+		 * depth 50인 재귀라..
+		 * 가지치기를 좀 해야하나? 
+		 * 1. T의 A 개수 < S의 A 개수  리턴..
+		 * 2. T의 길이 < S의 길이 리턴
+		 * 
+		 * 흠 이렇게 해도 14퍼에서 시간초과남
+		 * substr 넣어도 17에서 시간초과 힝 ㅠㅠ
+		 * 
+		 */
+		int res = (isChange)? 1 : 0;
+		System.out.println(res);
+	}
+	
+	private static boolean subStr(String s) {
+		boolean isSub = false;
+		for (int i = 0; i < T.length()-s.length() + 1; i++) {
+			String str = T.substring(i, i+s.length());
+			StringBuffer sb = new StringBuffer(str);
+			if (str.equals(s) || sb.reverse().toString().equals(s)) {
+				isSub = true;
+				break;
+			}
+		}
+		
+		return isSub;
+	}
+
+	private static void dfs(String str, int cntS) {
+		if (str.equals(T)) {
+			isChange = true;
+			return;
+		}
+		if (str.length() >= T.length()
+				|| cntS > cntT
+				|| !subStr(str)) return;
+		
+		//연산1
+		String newStr = str.concat("A");
+		dfs(newStr, cntS + 1);
+		
+		//연산2 - 문자열 추가 및 뒤집기에 string buffer 이용
+		StringBuffer sb = new StringBuffer(str);
+		sb.append("B").reverse();
+		newStr = sb.toString();
+		dfs(newStr, cntS);
+		
+	}
+	private static int getACnt(String str) {
+		int cnt = 0;
+		
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == 'A') cnt++;
+		}
+		
+		return cnt;
+	}
+	
+}

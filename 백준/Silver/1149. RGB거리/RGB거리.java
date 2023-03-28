2587fc7688b3a4defb6f96f954e2a641d74e7b9a
ksy00826 <76732805+ksy00826@users.noreply.github.com>
2023-03-28 21:52:49 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 14568 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int[][] cost;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(in.readLine());
+		cost = new int[N][3];
+		StringTokenizer st = null;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(in.readLine());
+			for (int j = 0; j < 3; j++) {
+				cost[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int[][] dp = new int[N][3];
+
+		dp[0][0] = cost[0][0];
+		dp[0][1] = cost[0][1];
+		dp[0][2] = cost[0][2];
+		for (int i = 1; i < N; i++) {
+			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + cost[i][0];
+			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + cost[i][1];
+			dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + cost[i][2];			
+		}
+		System.out.println(Math.min(dp[N-1][0], Math.min(dp[N-1][1], dp[N-1][2])));
+	}
+}

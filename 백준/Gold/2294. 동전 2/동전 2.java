74349f9cc763a85078d2c87503b40ace99acac87
김승연 <76732805+ksy00826@users.noreply.github.com>
2024-01-07 15:09:38 +0900

    [Gold V] Title: 동전 2, Time: 144 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++){
+            coins[i] = Integer.parseInt(in.readLine());
+        }
+
+        //dp logic
+        int[] dp = new int[K+1];
+        for (int j = 1; j < K+1; j++){
+            dp[j] = Integer.MAX_VALUE-1;
+        }
+        for (int n = 1; n < N+1; n++){
+            for (int k = coins[n-1]; k < K+1; k++){
+                dp[k] = Math.min(dp[k-coins[n-1]] + 1, dp[k]);
+            }
+        }
+
+//        System.out.println(Arrays.toString(dp));
+        System.out.println((dp[K] == Integer.MAX_VALUE-1)? -1 : dp[K]);
+    }
+}

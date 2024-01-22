6fdb00f526ed3bf042e21bad8eb0709d4bc24898
김승연 <76732805+ksy00826@users.noreply.github.com>
2024-01-22 16:35:00 +0900

    [Gold IV] Title: 알고스팟, Time: 188 ms, Memory: 16512 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -7,10 +7,9 @@ public class Main {
     static class Pos{
         int r, c;
         int depth;
-        public Pos(int r, int c, int depth){
+        public Pos(int r, int c){
             this.r = r;
             this.c = c;
-            this.depth = depth;
         }
     }
     static int N, M;
@@ -28,46 +27,37 @@ public class Main {
             }
         }
 
-//        dp = new int[N][M];
-//        for (int i = 0; i < N; i++){
-//            Arrays.fill(dp[i], Integer.MAX_VALUE -1);
-//        }
-        bfs(0, 0);
-//        for (int i = 0; i < N; i++){
-//            System.out.println(Arrays.toString(dp[i]));
-//        }
-//        System.out.println(dp[N-1][M-1]);
+        zeroOnebfs(new Pos(0, 0));
+        System.out.println(dist[N-1][M-1]);
     }
 
     static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
-    static int[][] dp;
-    private static void bfs(int i, int j) {
-        PriorityQueue<Pos> q = new PriorityQueue<>(new Comparator<Pos>() {
-            @Override
-            public int compare(Pos o1, Pos o2) {
-                return o1.depth - o2.depth;
-            }
-        });
-        boolean[][] visited = new boolean[N][M];
-        q.offer(new Pos(i, j, 0));
-        visited[i][j] = true;
+    static int[][] dist;
+    private static void zeroOnebfs(Pos start) {
+        //dec, dist
+        Deque<Pos> dec = new ArrayDeque();
+        dist = new int[N][M];
+        for (int i = 0; i < N; i++){
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
+        dist[start.r][start.c] = 0;
+        dec.add(start);
 
-        while(!q.isEmpty()){
-            Pos cur = q.poll();
-            if ((cur.r == N-1) && (cur.c == M-1)){
-                System.out.println(cur.depth);
-                return;
-            }
+        while(!dec.isEmpty()){
+            Pos cur = dec.pollFirst();
 
             for (int di = 0; di < 4; di++){
                 int nr = cur.r + dr[di];
                 int nc = cur.c + dc[di];
-                if (nr < 0 || nr >= N || nc < 0 || nc >= M || visited[nr][nc]) continue;
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
 
-                visited[nr][nc] = true;
-                if (map[nr][nc] == 1) q.offer(new Pos(nr, nc, cur.depth + 1));
-                else q.offer(new Pos(nr, nc, cur.depth));
+                //거리가 갱신되면
+                if (dist[nr][nc] > dist[cur.r][cur.c] + map[nr][nc]){
+                    dist[nr][nc] = dist[cur.r][cur.c] + map[nr][nc];
+                    if (map[nr][nc] == 0) dec.addFirst(new Pos(nr, nc));
+                    else dec.addLast(new Pos(nr, nc));
+                }
             }
         }
     }

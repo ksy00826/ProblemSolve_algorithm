f0c40e1c64706d0412d3669780075184ddef5fe1
김승연 <76732805+ksy00826@users.noreply.github.com>
2024-02-07 13:07:08 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 736 ms, Memory: 49448 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Pos{
+        int r, c;
+        public Pos(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(in.readLine());
+        Pos[] pos = new Pos[N];
+        for (int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            pos[i] = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+        Arrays.sort(pos, new Comparator<Pos>() {
+            @Override
+            public int compare(Pos o1, Pos o2) {
+                if (o1.c != o2.c) return o1.c - o2.c;
+                return o1.r - o2.r;
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for (Pos p : pos){
+            sb.append(p.r).append(" ").append(p.c).append("\n");
+        }
+        System.out.println(sb);
+    }
+}

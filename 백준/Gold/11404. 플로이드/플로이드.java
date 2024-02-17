e09a0a60458ab1e2278eaa0f1f3f5f898b01f9d0
김승연 <76732805+ksy00826@users.noreply.github.com>
2024-02-17 15:20:48 +0900

    [Gold IV] Title: 플로이드, Time: 416 ms, Memory: 42516 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,74 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static class Node{
-        int no, cost;
-        public Node(int no, int cost){
-            this.no = no;
-            this.cost = cost;
-        }
-    }
-    static int N, M;
-    static List<List<Node>> graph;
+    static int[][] graph;
+    static int INF = Integer.MAX_VALUE / 2 -1;
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(in.readLine());
-        M = Integer.parseInt(in.readLine());
-        graph = new ArrayList<>();
+        int N = Integer.parseInt(in.readLine());
+        int M = Integer.parseInt(in.readLine());
+        graph = new int[N+1][N+1];
         for (int i = 0; i < N+1; i++){
-            graph.add(new ArrayList<>());
+            Arrays.fill(graph[i], INF);
+            graph[i][i] = 0; //자기자신은 0
         }
-
-        while(M-- > 0){
+        while(M -- > 0){
             StringTokenizer st = new StringTokenizer(in.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            graph.get(a).add(new Node(b, cost));
+            graph[a][b] = Math.min(graph[a][b], cost); //노선이 여러개일 수 있다고 했으니까
         }
 
-        int[][] dist = new int[N+1][N+1];
-        for (int i = 0; i < N+1; i++){
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-
-        for (int i = 1; i < N+1; i++){
-            dijkstra(i, dist[i]);
+        //floyd-warshall
+        //경유지
+        for (int k = 1; k <= N; k++){
+            for (int i = 0; i <= N; i++){
+                for (int j = 0; j <= N; j++){
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < N+1; i++){
-            for (int j = 1; j < N+1; j++){
-                sb.append((dist[i][j] == Integer.MAX_VALUE)? 0 : dist[i][j]).append(" ");
+        for (int i = 1; i <= N; i++){
+            for (int j = 1; j <= N; j++){
+                sb.append((graph[i][j] == INF)? 0 : graph[i][j]).append(" ");
             }
             sb.append("\n");
         }
         System.out.println(sb);
-    }
-
-    private static void dijkstra(int start, int[] dist) {
-        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                return o1.cost - o2.cost;
-            }
-        });
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
-
-        while(!pq.isEmpty()){
-            Node cur = pq.poll();
-
-            for (Node adj : graph.get(cur.no)){
-                if (dist[adj.no] > dist[cur.no] + adj.cost){
-                    dist[adj.no] = dist[cur.no] + adj.cost;
-                    pq.offer(new Node(adj.no, dist[adj.no]));
-                }
-            }
-        }
     }
 }

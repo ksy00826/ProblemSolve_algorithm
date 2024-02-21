034cb14d39e69325255b73d30d7da044ad4b6513
김승연 <76732805+ksy00826@users.noreply.github.com>
2024-02-21 16:47:52 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,71 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        int M = Integer.parseInt(in.readLine());
+        boolean[][] graph = new boolean[N+1][N+1];
+        while(M-- > 0){
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = true;
+            graph[b][a] = true;
+        }
 
-	/*
-	 * 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 구하라
-	 * 
-	 * => 네크워크망을 입력받고 map 써서 bfs 하면 될듯
-	 * 
-	 */
-	static int N;
-	static Map<Integer, List<Integer>> net = new HashMap<>();
-	
-	public static void main(String[] args) {
-		//input
-		//한 줄당 최대 두개라서 그냥 스캐너 씀
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		int pairN = sc.nextInt();
-		
-		while(pairN-- > 0) {
-			int com1 = sc.nextInt();
-			int com2 = sc.nextInt();
-			putNet(com1, com2);
-			putNet(com2, com1);
-		}
-		
-		//logic
-		visited = new boolean[N+1]; //1~N -- index error
-		System.out.println(bfs(1));
-		
-	}
-	
-	static boolean[] visited;
-	private static int bfs(int curNode) {
-		int cnt = 0;
-		
-		Queue<Integer> q = new ArrayDeque<>();
-		q.offer(curNode);
-		visited[curNode] = true;
-		
-		while(!q.isEmpty()) {
-			int node = q.poll();
-			cnt++;
-			
-			for (int adj : net.get(node)) {
-				if (visited[adj]) continue;
-				
-				visited[adj] = true;
-				q.offer(adj);
-			}
-		}
-		return cnt-1;
-	}
+        boolean[] visited = new boolean[N+1];
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(1);
+        visited[1] = true;
 
-	private static void putNet(int key, int val) {
-		List<Integer> valLs;
-		if (net.containsKey(key)) 
-			valLs = net.get(key);
-		else 
-			valLs = new ArrayList<>();
-		valLs.add(val);
-		net.put(key, valLs);
-	}
+        int cnt = 0;
+        while(!q.isEmpty()){
+            int cur = q.poll();
+
+            for (int adj = 2; adj < N+1; adj++){
+                if (!graph[cur][adj] || visited[adj]) continue;
+
+                visited[adj] = true;
+                q.offer(adj);
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
 }
